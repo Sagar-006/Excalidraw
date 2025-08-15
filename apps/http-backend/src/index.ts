8f74b8308572693be87810c77afc787a18461a4d
Sagar-006 <sagarbiradar7030@gmail.com>
2025-08-15 19:04:58 +0530

    added htt-backend logic and some common packages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,73 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import { middleware } from './middleware';
+import { JWT_SECRET } from '@repo/backend-common/config';
+import {CreateSchema, SigninSchema, CreateRoomSchema} from '@repo/common/types'
 
 const app = express();
+app.use(express.json());
+
+interface userType {
+    username:string,
+    password:string,
+}
+    const users:any =  [];
+
+app.post("/signup",async(req,res) => {
+    
+    const body = CreateSchema.safeParse(req.body);
+
+    if(!body.success){
+        res.json({
+            message:"incorrect inputs"
+        })
+        return
+    }
+    
+    res.json({
+        message:"you are signed up",
+        users
+    })
+})
+
+app.post("/signin",async(req,res) => {
+    const parse = SigninSchema.safeParse(req.body);
+
+    if(!parse.success){
+        res.json({
+            message:"incorrect inputs"
+        })
+        return
+    }
+    
+
+    // const findUser = users.filter(((user:any) => user.username == username && user.password == password ));
+    const userId = 'as'
+    if(!userId) return 
+    
+        const token = jwt.sign({userId},JWT_SECRET)
+        
+        res.json({
+            message:"you are signin",
+            token,
+        })
+    
+
+    // res.json({
+    //     message:"you are signin",
+    //     findUser
+    // })
+})
+
+app.post("/room",middleware,(req,res) => {
+    console.log("on room route")
+    // @ts-ignore
+    const id = req.userId ;
+    console.log(id)
+
+    res.json({
+        id
+    })
+})
 
 app.listen(4000)
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from "@repo/backend-common/config";
+
+export const middleware =  (req:Request,res:Response,next:NextFunction) => {
+    const token = req.headers["authorization"] ?? "";
+    console.log(token)
+
+    const decoded =  jwt.verify(token,JWT_SECRET);
+    console.log(decoded)
+    
+
+    if(decoded){
+        // @ts-ignore
+        req.userId = decoded.userId;
+
+        
+        next();
+    }else{
+        return
+    }
+
+    
+}
