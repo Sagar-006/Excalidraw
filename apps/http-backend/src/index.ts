7bd072ab719a495f9ecd86ffa3d0e27433beb4e0
Sagar-006 <sagarbiradar7030@gmail.com>
2025-08-16 23:48:14 +0530

    completed http requests

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,104 +1,122 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import { middleware } from './middleware';
-import { JWT_SECRET } from '@repo/backend-common/config';
-import {CreateSchema, SigninSchema, CreateRoomSchema} from '@repo/common/types';
-import  {prismaClient} from '@repo/db/client'
+import express from "express";
+import jwt from "jsonwebtoken";
+import { middleware } from "./middleware";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import {
+  CreateSchema,
+  SigninSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
 
 const app = express();
 app.use(express.json());
 
-const Client =prismaClient;
+const Client = prismaClient;
 
 interface userType {
-    username:string,
-    password:string,
+  username: string;
+  password: string;
 }
-    const users:any =  [];
+const users: any = [];
 
-app.post("/signup",async(req,res) => {
-    
-    const body = CreateSchema.safeParse(req.body);
+app.post("/signup", async (req, res) => {
+  const parseData = CreateSchema.safeParse(req.body);
 
-    if(!body.success){
-        res.json({
-            message:"incorrect inputs"
-        })
-        return
-    }
+  if (!parseData.success) {
+    res.json({
+      message: "incorrect inputs",
+    });
+    return;
+  }
 
+  try {
     const createUser = await Client.user.create({
       data: {
-        email: req.body.email,
-        password: req.body.password,
-        name:req.body.name,
-        photo:req.body.photo,
+        email: parseData.data.email,
+        password: parseData.data.password,
+        name: parseData.data.name,
+        photo: parseData.data.photo,
       },
     });
-    
-    res.json({
-        message:"you are signed up!"
-        
-    })
-})
-
-app.post("/signin",async(req,res) => {
-    const parse = SigninSchema.safeParse(req.body);
-
-    if(!parse.success){
-        res.json({
-            message:"incorrect inputs"
-        })
-        return
-    }
-    const username = req.body.username
-
-    const findUser = await Client.user.findFirst({
-        where:{
-            email:req.body.email,
-            password:req.body.password,
-
-        }
-    })
-    
-        if(!findUser){
-            return
-        } 
-        console.log(findUser)
-    
-        const userId = findUser.id; 
-
-        const token = jwt.sign({userId},JWT_SECRET)
-      
-        res.json({
-            message:"you are signin",
-            token,
-        })
-    
-})
-
-app.post("/room",middleware,async(req,res) => {
-    console.log("on room route")
-    // @ts-ignore
-    const userId:any = req.userId ;
-    console.log(userId)
-
-    const addRoom = await Client.room.create({
-        data:{
-            adminId:userId,
-            slug:req.body.slug,
-
-
-        }
-    })
-
-    if(!addRoom){
-        return
-    }
 
     res.json({
-        message:"Room added successfully!"
-    })
-})
+      message: "you are signed up!",
+      userId: createUser.id,
+    });
+  } catch (e) {
+    res.json({
+      message: "User aleready exists!",
+    });
+  }
+});
 
-app.listen(4000)
+app.post("/signin", async (req, res) => {
+  const parseData = SigninSchema.safeParse(req.body);
+
+  if (!parseData.success) {
+    res.json({
+      message: "incorrect inputs",
+    });
+    return;
+  }
+
+  try {
+    const user = await Client.user.findFirst({
+      where: {
+        email: parseData.data.email,
+        password: parseData.data.password,
+      },
+    });
+
+    if (!user) {
+      res.json({
+        message: "user not found",
+      });
+      return;
+    }
+    console.log(user);
+
+    // const userId = findUser.id;
+
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET);
+
+    res.json({
+      message: "you are signin",
+      token,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.post("/room", middleware, async (req, res) => {
+  console.log("on room route");
+ try{
+   // @ts-ignore
+   const userId: any = req.userId;
+   console.log(userId);
+
+   const addRoom = await Client.room.create({
+     data: {
+       adminId: userId,
+       slug: req.body.slug,
+     },
+   });
+
+   if (!addRoom) {
+     return;
+   }
+
+   res.json({
+     message: "Room added successfully!",
+     roomId: addRoom.id,
+   });
+ }catch(e){
+    res.json({
+        message:"Room already exists with this name"
+    })
+ }
+});
+
+app.listen(4000);
